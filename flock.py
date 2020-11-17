76480ba3d74f86f8ec267418145742ee3f9a05d6
MKR <rafi867@gmail.com>
2020-11-18 08:50:54 +1100

    Dummy files deleted

--- flock.py
+++ flock.py
@@ -1,10 +1,32 @@
-from SomAgent import SomAgent
 import math
 from minisom import MiniSom
 import numpy as np
 import matplotlib.pyplot as plt
 import time
 
+class SomAgent:
+    def __init__(self, agent_id, som):
+        self.ID = agent_id
+        self.som = som
+        self.commHistory = []
+        print("Agent", self.ID, "created")
+
+    def updateComm(self, fromAgent , newComm):
+        updated = [fromAgent, newComm]
+        self.commHistory.append(updated)
+
+    def printComm(self):
+        print("Communication history for Agent ID:", self.ID)
+        print(self.commHistory)
+    def inputTracker(self,input,idx):
+        self.initInput = input
+        self.initID = idx
+        self.input = input
+
+    def getInput(self):
+        return self.input
+
+#####################################################################################################
 
 def convto2d (colors):
     size1d = colors.shape[0]
@@ -45,16 +67,17 @@ def fixed_decay(learning_rate, t, max_iter):
 for i in range(n_agents):
     s = MiniSom(xdim, ydim, 3, sigma = sigma, learning_rate=lrate, decay_function = fixed_decay)
     soms.append(s)
-    agent.append(SomAgent(i + 1, s))
+    a = SomAgent(i+1,s)
+    agent.append(a)
 
 # Creating Randomized Input Subsets
-colInputs = np.zeros((4, 400, 3))
+colInputs = np.zeros((400, 3))
 for i in range(n_agents):
     idx = np.random.choice(colors.shape[0], 400, replace=True)   #Generating 400 random indexe with replacement
-    colInputs[i] = colors[idx,:]
-    agent.inputTracker(colInputs, idx)
+    colInputs = colors[idx,:]
+    agent[i].inputTracker(colInputs, idx)
 
-# The Main Loop is not done
+# The Main Loop
 
 meetFlag = [0, 0, 0, 0]
 while (True):
@@ -63,7 +86,9 @@ while (True):
     for i in range(n_agents):
 
         plt.subplot(4, 3, plotCounter)
-        plt.imshow(abs(colors_2D), interpolation='none')
+        agentInput = agent[i].getInput();
+        agentInput2d = convto2d(agentInput)
+        plt.imshow(abs(agentInput2d), interpolation='none')
         plt.title('Input')
         plotCounter = plotCounter+1
 
@@ -73,13 +98,13 @@ while (True):
         plotCounter = plotCounter + 1
 
         # Training the SOM
-        agent[i].som.train_batch(colors, samples, verbose=True)
+        agent[i].som.train_batch(agentInput, samples, verbose=True)
 
         # After Training
         plt.subplot(4, 3, plotCounter)
         plt.imshow(abs(agent[i].som.get_weights()), interpolation='none')
         qE1 = agent[i].som.quantization_error(colors)
-        plt.title("Final," + " QE = " + str(round(qE1, 2)))
+        plt.title("Final," + " QE = " + str(round(qE1, 4)))
         plotCounter = plotCounter + 1
 
     plt.subplots_adjust(hspace=0.3)
@@ -88,13 +113,10 @@ while (True):
     #Communicate, which should include update
 
     chancetoMeet = np.random.rand(4)
-
-
     if chancetoMeet[0] > 0.5:
         print("Agent 1 and 2 are communicating")
         agent[0].updateComm(2,time.ctime())
         break
-
     if chancetoMeet[1] > 0.5:
         print("Agent 2 and 3 are communicating")
     if chancetoMeet[2] > 0.5:
